6d3f8e001984d6e49687983ae834c0ba2dc200a9
jacktsai <jack@777.tw>
2014-03-05 14:50:49 +0800

    調整PostController.php的存取控制，accessRules()。 調整Post的新建和更新操作。

--- protected/models/Tag.php
+++ protected/models/Tag.php
@@ -105,4 +105,26 @@ class Tag extends CActiveRecord
         {
                 return implode(', ',$tags);
         }
+
+	protected function afterSave()
+	{
+    		parent::afterSave();
+    		Tag::model()->updateFrequency($this->_oldTags, $this->tags);
+	}
+ 
+	private $_oldTags;
+ 
+	protected function afterFind()
+	{
+    		parent::afterFind();
+    		$this->_oldTags=$this->tags;
+	}
+
+	public function updateFrequency($oldTags, $newTags)
+        {
+                $oldTags=self::string2array($oldTags);
+                $newTags=self::string2array($newTags);
+                $this->addTags(array_values(array_diff($newTags,$oldTags)));
+                $this->removeTags(array_values(array_diff($oldTags,$newTags)));
+        }
 }
