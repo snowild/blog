239003bc13a247c571d29b811654e352f21449b7
jacktsai <jack@777.tw>
2014-03-06 15:58:58 +0800

    在views/post/view.php下面增加顯示Comment資料列表和添加Comment的區塊。 調整PostController.php的actionView()，在取得Post資料後，呼叫newComment()來判斷是否有送出Comment資料，如果有的話就將Comment資料儲存，並顯示成功添加Comment資料的訊息給使用者。

--- protected/controllers/PostController.php
+++ protected/controllers/PostController.php
@@ -47,9 +47,22 @@ class PostController extends Controller
 	public function actionView($id)
 	{
 		$post = $this->loadModel();
+		$comment = $this->newComment($post);
 		$this->render('view',array(
 			'model'=>$post,
 		));
+	}
+	
+	protected function newComment($post) {
+		$comment = new Comment();
+		if (isset($_POST['Comment'])) {
+			$comment->attributes = $_POST['Comment'];
+			if ($post->addComment($comment)) {
+				if ($comment->status==Comment::STATUS_PENDING) {
+					Yii::app()->user->setFlash('commentSubmitted', 'Thank you...');
+				}
+			}	
+		}	
 	}
 
 	private $_model;
