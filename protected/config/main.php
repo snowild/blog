587a522e3cd71a00a190c0f2fbf493878f5ea7e5
jacktsai <jack@777.tw>
2014-03-07 18:40:08 +0800

    將首頁改為Post的index.php。

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,8 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Yii Framework blog',
+    'defaultController'=>'post',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
